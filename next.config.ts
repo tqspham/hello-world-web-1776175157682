cc959da5563c60b16b9e23ffc2123d95f2d491f1
tqspham <tqs.pham@gmail.com>
2026-04-14 15:59:31 +0200

    Adding next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'loremflickr.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
